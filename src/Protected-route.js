3faa07bf38d8ed0c8273107dbf4aa134b854ade6
Vishal Ranjan <34218755+vishalrranjan@users.noreply.github.com>
2020-06-12 00:40:33 +0530

    added authentication method

--- src/Protected-route.js
+++ src/Protected-route.js
@@ -1,12 +1,26 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, Redirect } from "react-router-dom";
+import auth from "./Auth";
 
 export function ProtectedRoute({ component: Component, ...rest }) {
   return (
     <Route
       {...rest}
       render={props => {
-        return <Component {...props} />;
+        if (auth.isAuthenticated()) {
+          return <Component {...props} />;
+        } else {
+          return (
+            <Redirect
+              to={{
+                pathname: "/",
+                state: {
+                  from: props.location
+                }
+              }}
+            />
+          );
+        }
       }}
     />
   );
